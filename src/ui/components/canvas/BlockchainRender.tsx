3541604dda4ef77d28e9949d21933e230451aced
Vini Andrade <me@vicnicius.com>
2023-06-14 08:58:18 +0200

    Testing react-spring
    Signed-off-by: Vini Andrade <me@vicnicius.com>

--- src/ui/components/canvas/BlockchainRender.tsx
+++ src/ui/components/canvas/BlockchainRender.tsx
@@ -1,25 +1,20 @@
-import React, { FC, useEffect, useRef } from "react";
+import { useSpring, animated } from "@react-spring/three";
+import React, { FC } from "react";
 import { Chain } from "../../../domain/Block";
 import { Blockchain } from "../../../domain/Blockchain";
 import { BlockRender } from "./components/BlockRender";
-import { Group } from "three";
 
 export const BlockchainRender: FC<{
   chain: Blockchain<Chain.STX | Chain.BTC>;
   translateY?: number;
-}> = ({ chain, translateY }) => {
-  const groupRef = useRef<Group>(null!);
+}> = ({ chain }) => {
+  const [springs] = useSpring(() => ({ position: [0, 0] }));
   const { blocks } = chain;
-  useEffect(() => {
-    if (groupRef.current && translateY !== undefined) {
-      groupRef.current.translateY(translateY);
-    }
-  }, [translateY]);
   return (
-    <group ref={groupRef}>
+    <animated.group position={springs.position.to((x, y) => [x, y, 0])}>
       {Object.keys(blocks).map((id) => (
         <BlockRender key={id} block={blocks[id]} id={id} chain={chain} />
       ))}
-    </group>
+    </animated.group>
   );
 };
